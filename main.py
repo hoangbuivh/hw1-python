2e88ff407cadb529a2d5dd2d404a6da3e78858df
Hoang  Bui <hhb5051@psu.edu>
2020-09-04 20:00:13 +0000

    add author

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Author: Hoang Bui hhb5051@psu.edu
+
 grades = [["A", 4.0], ["A-", 3.67], ["B+", 3.33], ["B", 3.0], ["B-", 2.67], ["C+", 2.33], ["C", 2.0], ["D", 1.0], ["F", 0.0], ["F", 0.0], ["G", 0.0], ["H", 0.0], ["I", 0.0], ["J", 0.0],["K", 0.0], ["L", 0.0], ["M", 0.0], ["N", 0.0], ["O", 0.0], ["P", 0.0], ["Q", 0.0], ["R", 0.0],["S", 0.0], ["T", 0.0], ["U", 0.0], ["V", 0.0], ["W", 0.0], ["X", 0.0], ["Y", 0.0], ["Z", 0.0]]
 gradepointList = []
 creditpointList = []
@@ -8,8 +10,7 @@ for i in range(1,4):
   for grade in grades:
     if grade[0] == lettergrade: 
       gradepoint = grade[1]
-      gradepointList.append(gradepoint)
-
+      gradepointList.append(gradepoint)    
   print(f"Grade point for course {i} is: {gradepoint}")
 
 gpa  = (gradepointList[0]*creditpointList[0] + gradepointList[1]*creditpointList[1] + gradepointList[2]*creditpointList[2]) / (creditpointList[0] + creditpointList[1]+ creditpointList[2])
